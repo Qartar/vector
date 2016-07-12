db6936ea32c81ed9dd3e066bc1d6462adaf44f03
Carter Bray <qartar@live.com>
2016-07-12 16:12:22 -0700

    Add executable for comparing light models

--- src/trace/Image.cpp
+++ src/trace/Image.cpp
@@ -4,6 +4,10 @@
 
 bool WriteBitmapBGR(char const* filename, size_t width, size_t height, std::vector<uint8_t> const& pixels)
 {
+    //
+    //  initialize header
+    //
+
     DWORD file_size = (DWORD )(sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + pixels.size());
     DWORD data_size = (DWORD )pixels.size();
 
@@ -28,6 +32,22 @@ bool WriteBitmapBGR(char const* filename, size_t width, size_t height, std::vect
         0,                          //  biClrUsed
         0,                          //  biClrImportant
     };
+
+    //
+    //  create directory
+    //
+
+    auto fslash = std::strrchr(filename, '/');
+    auto bslash = std::strrchr(filename, '\\');
+    if (auto slash = std::max<char const*>(fslash, bslash)) {
+        std::vector<char> mut_filename(filename, slash + 1);
+        mut_filename[slash - filename] = '\0';
+        CreateDirectoryA(mut_filename.data(), NULL);
+    }
+
+    //
+    //  write file
+    //
 
     HANDLE hfile = CreateFileA(filename, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
 
---
+++ src/trace/Light.cpp
@@ -0,0 +1,45 @@
+#include "Trace.h"
+
+#include "math/Intrinsic.h"
+
+template<typename M, typename V, typename S, size_t Size>
+void test_models() {
+    Trace<M, V, S, L_Separable<Kd_Lambert, Ks_BlinnPhong>>(Size, Size, "light/lambert-blinnphong.bmp");
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_BlinnPhong>>(Size, Size, "light/disney-blinnphong.bmp");
+
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_BlinnPhong, F_Schlick, G_None>>>(Size, Size, "light/disney-blinnphong-schlick.bmp");
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_Beckmann, F_Schlick, G_None>>>(Size, Size, "light/disney-beckmann-schlick.bmp");
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_GGX, F_Schlick, G_None>>>(Size, Size, "light/disney-ggx-schlick.bmp");
+
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_BlinnPhong, F_Schlick, G_Implicit>>>(Size, Size, "light/disney-blinnphong-schlick-implicit.bmp");
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_Beckmann, F_Schlick, G_Implicit>>>(Size, Size, "light/disney-beckmann-schlick-implicit.bmp");
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_GGX, F_Schlick, G_Implicit>>>(Size, Size, "light/disney-ggx-schlick-implicit.bmp");
+
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_BlinnPhong, F_Schlick, G_Neumann>>>(Size, Size, "light/disney-blinnphong-schlick-neumann.bmp");
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_Beckmann, F_Schlick, G_Neumann>>>(Size, Size, "light/disney-beckmann-schlick-neumann.bmp");
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_GGX, F_Schlick, G_Neumann>>>(Size, Size, "light/disney-ggx-schlick-neumann.bmp");
+
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_BlinnPhong, F_Schlick, G_CookTorrance>>>(Size, Size, "light/disney-blinnphong-schlick-cook_torrance.bmp");
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_Beckmann, F_Schlick, G_CookTorrance>>>(Size, Size, "light/disney-beckmann-schlick-cook_torrance.bmp");
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_GGX, F_Schlick, G_CookTorrance>>>(Size, Size, "light/disney-ggx-schlick-cook_torrance.bmp");
+
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_BlinnPhong, F_Schlick, G_Kelemen>>>(Size, Size, "light/disney-blinnphong-schlick-kelemen.bmp");
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_Beckmann, F_Schlick, G_Kelemen>>>(Size, Size, "light/disney-beckmann-schlick-kelemen.bmp");
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_GGX, F_Schlick, G_Kelemen>>>(Size, Size, "light/disney-ggx-schlick-kelemen.bmp");
+
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_BlinnPhong, F_Schlick, G_GGX>>>(Size, Size, "light/disney-blinnphong-schlick-ggx.bmp");
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_Beckmann, F_Schlick, G_GGX>>>(Size, Size, "light/disney-beckmann-schlick-ggx.bmp");
+    Trace<M, V, S, L_Separable<Kd_Disney, Ks_CookTorrance<D_GGX, F_Schlick, G_GGX>>>(Size, Size, "light/disney-ggx-schlick-ggx.bmp");
+}
+
+int main()
+{
+    using M = intrinsic::Matrix;
+    using V = intrinsic::Vector;
+    using S = intrinsic::Scalar;
+    constexpr size_t Size = 1024;
+
+    test_models<M, V, S, Size>();
+
+    return 0;
+}
